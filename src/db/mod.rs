63eb7e6312efe197f483ec613153352b9815b62b
Cordeiro <danilocordeiro.ti@gmail.com>
2021-12-20 20:17:47 -0300

    Add db operations

---
+++ src/db/mod.rs
@@ -0,0 +1,5 @@
+mod db;
+mod manager;
+
+pub use db::{DB, UrlMap};
+pub use manager::{Manager, Message};
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
+use anyhow::Result;
 use crate::config::CONFIG;
-use crate::db::{DB, UrlMap};
+use crate::db::{DB, UrlMap, Message, Manager};   
 
 mod config;
 mod db;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     println!(
         "host: {}, port: {}, database.url: {}",
         CONFIG.host, CONFIG.port, CONFIG.database.url
     );
 
     let db = DB::new().await.unwrap();
-    let res = sqlx::query_as::<_, UrlMap>("select * from url_maps")
-        .fetch_all(&db.pool)
-        .await;
+    let (db_tx, db_rx) = tokio::sync::mpsc::channel(32);
+    tokio::spawn(async move {
+        let mut manager = Manager::new(db, db_rx);
+        manager.listen().await;
+    });
 
-    println!("results: {:?}", res);
+    let (tx, rx) = tokio::sync::oneshot::channel();
+    // match db_tx.send(Message::GetUrlMaps { resp: tx }).await {
+    //     Ok(_) => {},
+    //     Err(e) => eprintln!("Failed to send to database manager: {}", e)
+    // }
+
+    // match db_tx.send(Message::GetUrlMap { key: "github".into(), resp: tx }).await {
+    //     Ok(_) => {},
+    //     Err(e) => eprintln!("Failed to send to database manager: {}", e)
+    // }
+    
+    let url_map = UrlMap::new("linkedin".into(), "https://linkedin.com/danilocordeiro".into());
+    match db_tx.send(Message::CreateUrlMap {url_map , resp: tx }).await {
+        Ok(_) => {},
+        Err(e) => eprintln!("Failed to send to database manager: {}", e)
+    }
+
+    let url_maps = rx.await.unwrap();
+    match url_maps {
+        Ok(ums) => println!("url_maps: {:?}", ums),
+        Err(e) => eprintln!("Failed to send to database manager: {}", e)
+    }
+
+    Ok(())
+
 }
